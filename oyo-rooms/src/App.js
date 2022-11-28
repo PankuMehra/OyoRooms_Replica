b09906897d374e784784736ac1a882b62869ef61
Deepak Vats <deepakvats123123@gmail.com>
2022-11-28 18:11:04 +0530

    dependencies added

---
+++ oyo-rooms/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Checkout from './Componants/Checkout';
+import Payment from './Componants/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout />
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
