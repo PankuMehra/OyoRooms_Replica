df677fc28a1e5a1e52417bd1f86066036bfd27e8
komal kumar <er.kumarkomal@gmail.com>
2022-12-01 17:00:21 +0530

    responsiveness

--- product-details/src/components/DescNav/DescNav.js
+++ product-details/src/components/DescNav/DescNav.js
@@ -9,30 +9,36 @@ import "./DescNav.css";
 
 export default function DescNav() {
   return (
-    <div className="kk_nav">
-      <div>
-        <p>
-          <a href="#section0">About this OYO</a>
-        </p>
-        <p>
-          <a href="#section1">Amenities</a>
-        </p>
-        <p>
-          <a href="#section1">Choose your room</a>
-        </p>
-        <p>
-          <a href="#section1">Ratings and Reviews</a>
-        </p>
-        <p>
-          <a href="#section1">Hotel Rules</a>
-        </p>
-        <p>
-          <a href="#section1">What's nearby</a>
-        </p>
-      </div>
-      <div>
+    <>
+      <div className="collapse">
+        <button>=</button>
         <p>Share</p>
       </div>
-    </div>
+      <div className="kk_nav">
+        <div>
+          <p>
+            <a href="#section0">About this OYO</a>
+          </p>
+          <p>
+            <a href="#section1">Amenities</a>
+          </p>
+          <p>
+            <a href="#section1">Choose your room</a>
+          </p>
+          <p>
+            <a href="#section1">Ratings and Reviews</a>
+          </p>
+          <p>
+            <a href="#section1">Hotel Rules</a>
+          </p>
+          <p>
+            <a href="#section1">What's nearby</a>
+          </p>
+        </div>
+        <div>
+          <p>Share</p>
+        </div>
+      </div>
+    </>
   );
 }
