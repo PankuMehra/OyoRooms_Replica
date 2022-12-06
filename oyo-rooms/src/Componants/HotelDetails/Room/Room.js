4ed188043daf0d061455436e4c42aa7e48417361
PankuMehra <111267938+PankuMehra@users.noreply.github.com>
2022-12-06 22:29:22 +0530

    Delete oyo-rooms directory

--- oyo-rooms/src/Componants/HotelDetails/Room/Room.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./Room.css";
-export default function Room() {
-  return <div></div>;
-}
--- oyo-rooms/src/URL.js
+++
@@ -1,4 +0,0 @@
-export const URL = {
-    hotel : "http://localhost:3000/hotel",
-    users : "http://localhost:3000/users"
-}
